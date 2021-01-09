9a2192739335beeea5099ad514ed553ef17379c9
Nizar Rian <garudaindonesia710@gmail.com>
2021-01-09 14:43:07 +0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,7 +17,8 @@ def echo(update, context):
     url = 'https://secureapp.simsimi.com/v1/simsimi/talkset?uid=297390035&av=6.9.3.7&lc=id&cc=ID&tz=Asia%2FJakarta&os=a&ak=Nsh1x94iNA2oftvixJMmTj1awEk%3D&message_sentence={}&normalProb=8&isFilter=1&talkCnt=8&talkCntTotal=8&reqFilter=1&session=MMWBpntzK2hS64aX7SuhQHTcsHCrVftmwJBk7cGd3ViCyVTFx4ywxuEvvTHnQWrt9ENooUhdQXaD6XrDuTyGbSv2&triggerKeywords=%5B%5D'.format(update.effective_message.text)
     r = requests.get(url)
     data = json.loads(r.text)
-    update.effective_message.reply_text(data['simsimi_talk_set']['answers'][0]['sentence'])
+    # update.effective_message.reply_text(data['simsimi_talk_set']['answers'][0]['sentence'])
+    update.effective_message.reply_text("Bot mati dulu ya gan")
 
 updater = Updater('1459712147:AAGIk8Q6DNyl-HbgtsbuP7u2AnItfIESqJE',
 use_context=True)
@@ -28,5 +29,5 @@ updater.dispatcher.add_handler(CommandHandler('donasi', donasi))
 echo_handler = MessageHandler(Filters.text, echo)
 updater.dispatcher.add_handler(echo_handler)
 
-updater.start_polling(
+updater.start_polling()
 updater.idle()
